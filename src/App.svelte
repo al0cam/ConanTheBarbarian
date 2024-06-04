8f7b045f0a17bedfca2cbb76801762d3de2147d3
Al0cam <benjaminsikac@gmail.com>
2024-06-04 17:11:02 +0200

    added opacity instead of hovered

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,10 @@
 type Item = {
   id: number,
   text: string,
-  hovered: boolean
+  opacity: string
 }
 
-// TODO: add base classes for the items
+// TODO: add base css classes for the items
 // TODO: add change of color on items that arent hovered while one is
 // TODO: add animations to the items
 // TOOD: add background images
@@ -15,43 +15,51 @@ let items: Item[] = [
   {
     id: 1,
     text: "To crush your enemies,",
-    hovered: false
+    opacity: "opacity-100"
   },
   {
     id: 2,
     text: "see them driven before you,",
-    hovered: false
+    opacity: "opacity-100"
   },
   {
     id: 3,
     text: "and to hear the lamenetation of their women!",
-    hovered: false
+    opacity: "opacity-100"
   }
 ]
 
-function changeOpactiy(item: Item) {
+function changeOpactiy(item: Item){
   items = items.map(i => {
-    if(i.id === item.id) {
+    if(i.id === item.id){
       return {
         ...i,
-        hovered: true
+        opacity: "opacity-100"
       }
     } else {
       return {
         ...i,
-        hovered: false
+        opacity: "opacity-35"
       }
     }
   })
 }
 
+function resetOpacity() {
+  items = items.map(i => {
+    return {
+      ...i,
+      opacity: "opacity-100"
+    }
+  })
+}
 
 </script>
 
 <main id="menu" class="m-0 h-screen flex gap-4 items-center">
-  <div id="items" class="flex flex-col gap-5" >
+  <div id="items" class="flex flex-col gap-5" on:mouseleave={() => resetOpacity()}>
     {#each items as item}
-        <div class="text-5xl {item.hovered ? 'opacity-100' : 'opacity-5'}" on:mouseover={()=> changeOpactiy(item)}>{item.text}</div>
+        <div class="transition ease-in-out duration-200 text-5xl {item.opacity}" on:mouseover={()=> changeOpactiy(item)}>{item.text}</div>
     {/each}
   </div>
 </main>
